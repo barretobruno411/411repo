3d2db3624d59ab8c3d2a2c719380c00c2ec4e351
Bruno Duarte <bruno.duarte411@gmail.com>
2022-07-12 22:56:25 -0300

    selecting just one item

--- src/components/Submit.js
+++ src/components/Submit.js
@@ -1,27 +1,49 @@
-import React, { useEffect, useState } from 'react';
-import Item from './Item';
+import React, { useEffect, useState } from "react";
+import Item from "./Item";
 
-function Submit(props){
+function Submit(props) {
+  const numbers = ["1", "2", "3", "4", "5"];
+  const [rating, setRating] = useState(
+    numbers.map((number) => {
+      return (number = new Item(number));
+    })
+  );
 
-    const numbers = ["1","2","3","4","5"];
-    const [rating, setRating] = useState(numbers.map(number=>{
-        return number = new Item(number);
-    }));
+  useEffect(() => {
+    console.log(rating);
+  }, [rating]);
 
-    useEffect(()=>{
-        console.log(rating)
-    },[rating])
+  function onSelect(item) {
+    console.log(item);
+    const newRating = rating.map((prevItem) => {
+      if (prevItem.value === item.value) {
+        prevItem.checked = true;
+      } else {
+        prevItem.checked = false;
+      }
 
-    return(
-        <div className="submit">
-            <ul>
-                {rating.map(item => <li>{item.value}</li>)}
-            </ul>
-            <button className="submittButton">
-                SUBMIT
-            </button>
-        </div>
-    )
+      return prevItem;
+    });
+
+    setRating(newRating);
+  }
+
+  return (
+    <div className="submit">
+      <ul>
+        {rating.map((item) => (
+          <li
+            onClick={(event) => {
+              onSelect(item);
+            }}
+          >
+            {item.value}
+          </li>
+        ))}
+      </ul>
+      <button className="submittButton">SUBMIT</button>
+    </div>
+  );
 }
 
 export default Submit;
--- src/components/TextContent.js
+++ src/components/TextContent.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React from "react";
+import imagem from "../images/icon-star.svg";
 
-function TextContent(props){
-    return(
-        <div className="textContent">
-            <h1>How did we do?</h1>
-            <p>please let us know how we did with your<br/>
-            suport request. All feedback is appreciated<br/>
-            to help us improve our offering!</p>
-        </div>
-    )
+function TextContent(props) {
+  return (
+    <div className="textContent">
+      <div className="star-container">
+        <img src={imagem} alt="star" />
+      </div>
+      <h1>How did we do?</h1>
+      <p>
+        please let us know how we did with your
+        <br />
+        suport request. All feedback is appreciated
+        <br />
+        to help us improve our offering!
+      </p>
+    </div>
+  );
 }
 
 export default TextContent;
